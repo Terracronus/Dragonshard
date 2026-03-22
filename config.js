8f720f00fba1554adfed924fb1e9752f7d273b1c
Terracronus <Carlofreiria@hotmail.es>
2026-03-22 19:40:13 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4wjtRUU5eTxBKMrYp615Ynqo2c5LSjsI",
+  authDomain: "terracronus-dragonshard.firebaseapp.com",
+  projectId: "terracronus-dragonshard",
+  storageBucket: "terracronus-dragonshard.firebasestorage.app",
+  messagingSenderId: "479723332460",
+  appId: "1:479723332460:web:2ea5b21c67ddc70b018949"
+};
